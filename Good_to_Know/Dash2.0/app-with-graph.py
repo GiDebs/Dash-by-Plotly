6c5637ca9d411e9d20a9c0eed9724566c3e01f19
GiDebs <giulio.deba@gmail.com>
2022-08-20 22:05:10 +0200

    addng notes

--- Good_to_Know/Dash2.0/app-with-graph.py
+++ Good_to_Know/Dash2.0/app-with-graph.py
@@ -1,16 +1,16 @@
-# To understand the meaning oth the libraries CTRL + right click
+# To understand the meaning oth the libraries or objects CTRL + left click on the object
 
 from dash import Dash, dcc, Output, Input  # pip install dash
 import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
 import plotly.express as px                # library that allows me to build graphs
 
 # incorporate data into app
-df = px.data.medals_long() #data are coming from this function which is a built-in dataset for educational purposes in data library 
+df = px.data.medals_long() #data are coming from this function which is a built-in dataset for educational purposes inside plotly express library 
 
 # Build your components
 app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
 mytitle = dcc.Markdown(children='# App that analyzes Olympic medals')
-mygraph = dcc.Graph(figure={})
+mygraph = dcc.Graph(figure={}) #It's an empty figure right now that will be populated using the callback
 dropdown = dcc.Dropdown(options=['Bar Plot', 'Scatter Plot'],
                         value='Bar Plot',  # initial value displayed when page first loads
                         clearable=False)
--- Good_to_Know/Dash2.0/dash2.py
+++ Good_to_Know/Dash2.0/dash2.py
@@ -1,5 +1,3 @@
-# If you prefer to run the code online instead of on your computer click:
-# https://github.com/Coding-with-Adam/Dash-by-Plotly#execute-code-in-browser
 
 from dash import Dash, dcc, Output, Input  # pip install dash
 import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
@@ -15,16 +13,16 @@ print(df.head())
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 mytitle = dcc.Markdown(children='')
 mygraph = dcc.Graph(figure={})
-dropdown = dcc.Dropdown(options=df.columns.values[2:],
+dropdown = dcc.Dropdown(options=df.columns.values[2:],   #consider only the columns from the number 2 to the end, starting trom 0
                         value='Cesarean Delivery Rate',  # initial value displayed when page first loads
                         clearable=False)
 
 # Customize your own Layout
 app.layout = dbc.Container([
-    dbc.Row([
+    dbc.Row([                            #In the first row I write what's inside mytitle collecting 6 columns and centering it
         dbc.Col([mytitle], width=6)
     ], justify='center'),
-    dbc.Row([
+    dbc.Row([                            #In the second row I write what's inside mygraph collecting 12 columns (all width of the screen)
         dbc.Col([mygraph], width=12)
     ]),
     dbc.Row([
@@ -35,7 +33,7 @@ app.layout = dbc.Container([
 
 # Callback allows components to interact
 @app.callback(
-    Output(mygraph, 'figure'),
+    Output(mygraph, 'figure'),    #because I have 2 Output I will need to return 2 variables!
     Output(mytitle, 'children'),
     Input(dropdown, 'value')
 )
@@ -44,7 +42,7 @@ def update_graph(column_name):  # function arguments come from the component pro
     print(column_name)
     print(type(column_name))
     # https://plotly.com/python/choropleth-maps/
-    fig = px.choropleth(data_frame=df,
+    fig = px.choropleth(data_frame=df,        # This plotly express built in function create the beautiful graph
                         locations='STATE',
                         locationmode="USA-states",
                         scope="usa",
@@ -53,7 +51,7 @@ def update_graph(column_name):  # function arguments come from the component pro
                         animation_frame='YEAR')
 
     return fig, '# '+column_name  # returned objects are assigned to the component property of the Output
-
+                                 # the '#' is because they are concatenated to the variable since shoud be an header
 
 # Run app
 if __name__=='__main__':
